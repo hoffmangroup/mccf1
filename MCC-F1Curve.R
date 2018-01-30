1d1a3519fda56bf90f547a3f3f130bc3bded38d2
kirin cao <kirin.cao@mail.utoronto.ca>
2018-01-30 16:15:30 -0500

    changed some naming in MCC-F1Curve file

---
+++ MCC-F1Curve.R
@@ -0,0 +1,109 @@
+library(ROCR)
+library(ggplot2)
+
+mccf1_calcu <- function(actualVector, predictedVector, fold=100) {
+  # actualVector is a vector of actual values in binary classification
+  # predictedVector is a vector of predicted values in binary classification
+  # fold is the number that will be used to divide the range of normalized Matthews Correlation Coefficient
+  
+  # get a performance object based on the classification
+  pred <- prediction(predictedVector, actualVector)
+  perf <- performance(pred, measure = "mat", x.measure = "f")
+  # get MCC (Matthews correlation coefficient)
+  mcc <- attr(perf, "y.values")[[1]]
+  # transform MCC to normalised MCC: change the range of MCC from [-1, 1] to [0, 1]
+  mcc.nor <- (mcc + 1) / 2
+  # get F1 score
+  f1 <- attr(perf, "x.values")[[1]]   
+  # get the thresholds
+  thresholds <- attr(perf, "alpha.values")[[1]]
+  
+  # get rid of NaN values in the vectors of mcc.nor and f1
+  mcc.nor_truncated <- mcc.nor[2: (length(mcc.nor) - 1)]
+  f_truncated <- f1[2: (length(f1) - 1)]
+  
+  # calculate mcc_f1 metric
+  sum_of_mean_distance <- c()
+  unit_len <- (max(mcc.nor_truncated) - min(mcc.nor_truncated)) / fold
+  for (i in 1:fold){
+    # find all the points with mcc between unit_len*(i-1) and unit_len*i
+    pos1 <- which(mcc.nor_truncated >= min(mcc.nor_truncated) + (i-1) * unit_len)
+    pos2 <- which(mcc.nor_truncated <= min(mcc.nor_truncated) + i * unit_len)
+    pos <- c()
+    for (m in pos1){
+      if  (m %in% pos2){
+        pos <- c(pos, m)
+      }
+    }
+    sum_of_distance <- 0
+    for (j in pos){
+      temp_dist <- sqrt((mcc.nor_truncated[j] - 1)^2 + (f_truncated[j] - 1)^2)  
+      sum_of_distance <- sum_of_distance + temp_dist
+    }
+    
+    sum_of_mean_distance <- c(sum_of_mean_distance, sum_of_distance / length(pos))
+  }
+  
+  sums_no_na <- sum(sum_of_mean_distance, na.rm=T)
+  mccf1_metric <- 1 - (sums_no_na / length(sum_of_mean_distance)) / sqrt(2)
+
+  # find the best threshold 
+  eu_distance = c()
+  for (i in (1:length(mcc.nor))){
+    eu_distance <- c(eu_distance, sqrt((1 - mcc.nor[i])^2 + (1 - f1[i])^2))
+  }
+  
+  bestThreshold <- thresholds[match(min(eu_distance, na.rm = T), eu_distance)]
+  
+  # output of the function is the MCC-F1 metric and the top threshold
+  mccf1_result <- list(mccf1_metric = mccf1_metric, bestThreshold = bestThreshold)
+  return(mccf1_result)
+}
+
+
+mccf1_plot <- function(actualVector, predictedVector, fold=100, .title="the MCC-F1 score curve",
+                       .curveFileName) {
+  # actualVector is a vector of actual values in binary classification
+  # predictedVector is a vector of predicted values in binary classification
+  # fold is the number that will be used to divide the range of normalized Matthews Correlation Coefficient
+  # title is the title of the plot of the MCC-F1 curve
+  # if the user wants to save the pdf file of the graph of the MCC-F1 curve automatically, 
+  # curveFileName is the location and name of the curve 
+  
+  # get a performance object based on the classification
+  pred <- prediction(predictedVector, actualVector)
+  perf <- performance(pred, measure = "mat", x.measure = "f")
+  # get MCC (Matthews correlation coefficient)
+  mcc <- attr(perf, "y.values")[[1]]
+  # get normalised MCC: change the range of MCC from [-1, 1] to [0, 1]
+  mcc.nor <- (mcc + 1) / 2
+  # get F1 score
+  f1 <- attr(perf, "x.values")[[1]]   
+  # get the thresholds
+  thresholds <- attr(perf, "alpha.values")[[1]]
+  
+  mccf1_df <- data.frame(F1 = f1, MCC.nor = mcc.nor) 
+  
+  # plot the MCC-F1 curve
+  mccf1_plot = ggplot(mccf1_df, aes(x = F1, y = MCC.nor, ymin = 0, ymax = 1, xmin = 0, xmax = 1)) + 
+    geom_point(size = 0.2, shape = 21, fill = "white")+
+    theme(plot.title = element_text(hjust = 0.5))+
+    coord_equal(ratio = 1)+
+    labs(x = "F1 score", y = "normalized MCC", title = .title)
+  
+  # xlab(expression(F[1]*" score"))
+  
+    plot(mccf1_plot)
+  
+  # if the user specifies the location which the plot should be saved to, then produce a pdf file of the 
+  # plot and save it to the location
+  if (!missing(.curveFileName)){
+    pdf(file = .curveFileName)
+    plot(mccf1_plot)
+    dev.off()
+  }
+}
+
+
+
+
